2367bed28cfee91b398e842684619dc6446b1a7d
shahmeerdevdex <128673394+AliGates915@users.noreply.github.com>
2026-02-17 17:29:19 +0500

    ssa

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,19 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 
+const quotes = [
+  "The greatest glory in living lies not in never falling, but in rising every time we fall. - Nelson Mandela",
+  "The way to get started is to quit talking and begin doing. - Walt Disney",
+  "Your time is limited, so don't waste it living someone else's life. - Steve Jobs",
+  "If life were predictable it would cease to be life, and be without flavor. - Eleanor Roosevelt",
+  "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough. - Oprah Winfrey",
+];
 
+app.get("/quotes", (req: Request, res: Response) => {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  const randomQuote = quotes[randomIndex];
+  return res.json({ quote: randomQuote });
+});
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
 
